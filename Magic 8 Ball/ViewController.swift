606837d86d85140a0220ef3ca45d294ed7ffb601
Mikita Kryvitski <solairerove@gmail.com>
2019-05-03 15:57:01 +0300

    feature: create simple magic ball application

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+
+    @IBOutlet weak var ballImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeBallImageView()
     }
 
+    @IBAction func askButtonEmit(_ sender: UIButton) {
+        changeBallImageView()
+    }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        changeBallImageView()
+    }
+
+    func changeBallImageView() {
+        let ballImageViewRandomIndex = getRandomIndex(arraySize: ballArray.count)
+
+        ballImageView.image = UIImage(named: ballArray[ballImageViewRandomIndex])
+    }
+
+    func getRandomIndex(arraySize: Int) -> Int {
+        return Int.random(in: 0 ... arraySize - 1)
+    }
 }
-
