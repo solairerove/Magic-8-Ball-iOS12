a22ac6bdd3d3734174509aaff3cf388d4ea4e651
Mikita Kryvitski <solairerove@gmail.com>
2019-05-03 15:27:58 +0300

    feature: set up new project

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Nikita Krivitski on 5/3/19.
+//  Copyright © 2019 Nikita Krivitski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
